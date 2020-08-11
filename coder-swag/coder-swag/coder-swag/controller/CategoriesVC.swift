86d93e1d9994fccada0cf50dc5663f6d78c0c36d
Xanathos <xanathosd2x@gmail.com>
2020-08-10 21:02:16 -0500

    coder swag start

---
+++ coder-swag/coder-swag/coder-swag/controller/CategoriesVC.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  coder-swag
+//
+//  Created by Christian Torrico on 7/25/20.
+//  Copyright © 2020 Christian Torrico. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesVC: UIViewController {
+
+    @IBOutlet weak var tableView : UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let cat = Category()
+        cat.title = "Blah"
+        
+    }
+
+
+}
+
---
+++ coder-swag/coder-swag/coder-swag/model/Category.swift
@@ -0,0 +1,14 @@
+//
+//  Category.swift
+//  coder-swag
+//
+//  Created by Christian Torrico on 7/27/20.
+//  Copyright © 2020 Christian Torrico. All rights reserved.
+//
+
+import Foundation
+
+struct Category {
+    public var title: String!
+    public var imageName: String!
+}
