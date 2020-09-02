056f333b0ebcdb150fdc6022eacb331b4a7a8944
Xanathos <xanathosd2x@gmail.com>
2020-09-01 23:58:01 -0500

    smack intro

---
+++ Smack/Smack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by Christian Torrico on 9/1/20.
+//  Copyright © 2020 Christian Torrico. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
